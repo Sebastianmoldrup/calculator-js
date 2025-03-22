828b5e58e56376750049d4efa9e437863e5bda33
Sebastian Home <sebastianmoldrup@Sebastians-Air.home>
2025-03-22 22:48:29 +0100

    refactored and updated code

--- app/index.js
+++ app/index.js
@@ -2,77 +2,112 @@
 const display = document.querySelector("#display");
 const expression = document.querySelector("#expression");
 const buttons = document.querySelectorAll("button");
+// Get the seperate buttons and map to store the text value and data value
+const controls = [...document.querySelectorAll("[data-type='controls']")].map(
+  (control) => ({
+    value: control.dataset.value,
+    visual: control.textContent.trim(),
+  }),
+);
+const numbers = [...document.querySelectorAll("[data-type='number']")].map(
+  (number) => ({
+    value: number.dataset.value,
+    visual: number.textContent.trim(),
+  }),
+);
+const operators = [...document.querySelectorAll("[data-type='operator']")].map(
+  (operator) => ({
+    value: operator.dataset.value,
+    visual: operator.textContent.trim(),
+  }),
+);
 
+// Input array
 let input = [];
-const operatorKeys = ["÷", "×", "−", "+", "%", "DEL"];
-const operators = {
-  "×": "*",
-  "÷": "/",
-  "%": "%",
-  "+": "+",
-  "−": "-",
-};
 
-// Click function to handle event
-const handleClick = (e) => {
-  const val = e.target.innerText.trim();
-  const currOperator = operatorKeys.includes(val);
-  const prevOperator = operatorKeys.includes(input[input.length - 1]);
+const handleInput = (e) => {
+  // Get all buttons and store in an array
+  const buttonList = [...controls, ...numbers, ...operators];
+  // Store the value of the button clicked
+  const value = e.target.dataset.value;
+  // Find the button clicked
+  const button = buttonList.find((b) => b.value === value);
 
-  // Calculate equation
-  if (val === "=" && input.length) {
-    handleCalculation();
+  // Sanitize for XSS
+  if (!sanitize(button.value)) {
+    throw new Error("I don't recognize this input..🤨");
+  }
+
+  // Backspace previous input
+  if (button.value === "backspace") {
+    backspace();
     return;
   }
 
   // Clear display
-  if (val === "AC") {
-    display.innerText = "0";
-    expression.innerText = "";
+  if (button.value === "clear") {
+    clearDisplay();
+    return;
+  }
+
+  // Calculate
+  if (button.value === "=") {
+    calculate();
+    return;
+  }
+
+  // Display result
+  input.push(button.value);
+  display.textContent = input.join("");
+};
+
+const clearDisplay = () => {
+  input = [];
+  display.textContent = "0";
+  return;
+};
+
+const backspace = () => {
+  if (input.length === 0) {
+    return;
+  } else if (input.length === 1) {
     input = [];
+    display.textContent = "0";
     return;
   }
-
-  // Handle exponentation value
-  if (input.length && val === "DEL") {
-    input.pop();
-    input.length
-      ? (display.innerText = input.join(""))
-      : (display.innerText = "0");
-    return;
-  }
-
-  // Sanitize for XSS
-
-  // First value must be of type number or string "-"
-  if (!input.length && currOperator && val !== "−") {
-    return;
-  }
-
-  // Dont allow stacking operators
-  if (prevOperator && currOperator) {
-    return;
-  }
-
-  // Push value to input array && add input to display
-  input.push(val);
-  display.innerText = input.join("");
+  input.pop();
+  display.textContent = input.join("");
+  return;
 };
 
-// Calculate function
-const handleCalculation = () => {
-  const tokens = input
-    .map((i) => {
-      if (Object.keys(operators).includes(i)) {
-        return operators[i];
-      }
-      return i;
-    })
-    .join("");
+const sanitize = (val) => {
+  const isControls = controls.some((control) => control.value === val);
+  const isNumber = numbers.some((number) => number.value === val);
+  const isOperator = operators.some((operator) => operator.value === val);
 
-  display.innerText = eval(tokens);
-  expression.innerText = input.join("");
+  return isControls || isNumber || isOperator;
 };
 
-// Event listener for each button
-buttons.forEach((button) => button.addEventListener("click", handleClick));
+const calculate = () => {
+  // Safeguard for calculate if the last input is an operator
+  if (operators.includes(input[input.length - 1])) {
+    return;
+  }
+
+  // Run calculation of input string using eval()
+  const ans = eval(input.join(""));
+  // Update display with result
+  display.textContent = ans;
+  // Update expression with input
+  expression.textContent = input.join("");
+
+  // Reset input array
+  input = [];
+  // Add result to input array
+  input = ans.toString().split("");
+  return;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleInput);
+});
